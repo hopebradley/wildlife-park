5893032998875cddcd48dbcd80e8628816cecab1
Hope Bradley <hope.e.bradley@gmail.com>
2021-06-04 13:23:17 -0400

    finish homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,12 @@
+import {Link} from 'react-router-dom';
 
 const HomePage = (props) => {
 
     return (
         <div id="homepage">
             <h1>Welcome to Wildlife Park!</h1>
-            <button className="home-button">SEE HABITATS</button>
-            <button className="home-button">SEE ANIMALS</button>
+            <button className="home-button"><Link to="/habitats">SEE HABITATS</Link></button>
+            <button className="home-button"><Link to="/animals">SEE ANIMALS</Link></button>
         </div>
     )
 }
