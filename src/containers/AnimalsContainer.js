b7d7f2cbc6c9644f65b0b833130ba89dcf6d69ca
Hope Bradley <hope.e.bradley@gmail.com>
2021-06-05 23:08:19 -0400

    make animalscontainer a class component

--- src/containers/AnimalsContainer.js
+++ src/containers/AnimalsContainer.js
@@ -1,42 +1,58 @@
 import React from 'react';
 import Animal from '../components/Animal'
 import { Link } from 'react-router-dom'
+import { render } from '@testing-library/react';
 
-const AnimalsContainer = (props) => {
+class AnimalsContainer extends React.Component {
 
-    const animals = props.animals
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        console.log(e.target);
-        // const selected = e.target.children[0].selectedIndex.value;
-        // if (selected == "A-Z") {
-        //     animals = animals.sort((a, b) => (a.name > b.name) ? 1 : -1 )
-        //     this.render()
-        // }
+    state = {
+        animals: this.props.animals
     }
 
-    return (
-        <div id="animals-container">
-            <button className="button"><Link to="/add-animal">Add An Animal</Link></button>
-            <div className="sort">
-                <h3>SORT:</h3>
-                <form onSubmit={handleChange}>
-                    <select>
-                            <option>A-Z</option>
-                            <option>Activity</option>
-                            <option>Habitat</option>
-                    </select>
-                    <input type="submit"/>
-                </form>
-                
+    handleChange = (e) => {
+        e.preventDefault();
+        const select = e.target.children[0];
+        const index = select.selectedIndex;
+        const value = select[index].innerText;
+        let newAnimalOrder;
+        if (value == "Name (A-Z)") {
+            newAnimalOrder = this.state.animals.sort((a, b) => (a.name > b.name) ? 1 : -1 ); 
+        }
+        else if (value == "Activity") {
+            newAnimalOrder = this.state.animals.sort((a, b) => (a.active < b.active) ? 1 : -1 ); 
+        }
+        else {
+            newAnimalOrder = this.state.animals;
+        }
+        this.setState({
+            animals: newAnimalOrder
+        })
+    }
+
+    render() {
+        return (
+            <div id="animals-container">
+                {console.log(this.state.animals)}
+                <button className="button"><Link to="/add-animal">Add An Animal</Link></button>
+                <div className="sort">
+                    <h3>SORT:</h3>
+                    <form onSubmit={this.handleChange}>
+                        <select>
+                                <option>Name (A-Z)</option>
+                                <option>Activity</option>
+                        </select>
+                        <input type="submit"/>
+                    </form>
+                </div>
+                {this.state.animals.map(a => {
+                    const h = this.props.habitats.find(h => h.id == a.habitat_id).name;
+                    return <Animal animal={a} habitat={h}/>
+                })}
             </div>
-            {animals.map(a => {
-                const h = props.habitats.find(h => h.id == a.habitat_id).name;
-                return <Animal animal={a} habitat={h}/>
-            })}
-        </div>
-    )
+        )
+
+    }
+
 }
 
 export default AnimalsContainer
