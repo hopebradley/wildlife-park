7dd1996101460460701088ea2b17b4a61b6e908a
Hope Bradley <hope.e.bradley@gmail.com>
2021-06-05 23:18:06 -0400

    add habitat filtering

--- src/containers/HabitatsContainer.js
+++ src/containers/HabitatsContainer.js
@@ -8,11 +8,49 @@ class HabitatsContainer extends React.Component {
         habitats: this.props.habitats
     }
 
+    handleChange = (e) => {
+        e.preventDefault();
+        const select = e.target.children[0];
+        const index = select.selectedIndex;
+        const value = select[index].innerText;
+        let newHabitatOrder;
+        if (value == "Sort By Name (A-Z)") {
+            newHabitatOrder = this.state.habitats.sort((a, b) => (a.name > b.name) ? 1 : -1 ); 
+        }
+        else if (value == "Aquatic") {
+            newHabitatOrder = this.state.habitats.filter((h) => h.aquatic); 
+        }
+        else if (value == "Indoor") {
+            newHabitatOrder = this.state.habitats.filter((h) => h.indoor); 
+        }
+        else if (value == "Outdoor") {
+            newHabitatOrder = this.state.habitats.filter((h) => !h.indoor); 
+        }
+        else {
+            newHabitatOrder = this.state.habitats;
+        }
+        this.setState({
+            habitats: newHabitatOrder
+        })
+    }
+
     render() {
         return (
             <div>
                 <button className="button">Create A New Habitat!</button>
-                {this.state.habitats.map(h => <Habitat habitat={h} animals={props.animals} />)}
+                <div className="sort">
+                    <h3>FILTER:</h3>
+                    <form onSubmit={this.handleChange}>
+                        <select>
+                                <option>Sort By Name (A-Z)</option>
+                                <option>Aquatic</option>
+                                <option>Indoor</option>
+                                <option>Outdoor</option>
+                        </select>
+                        <input type="submit"/>
+                    </form>
+                </div>
+                {this.state.habitats.map(h => <Habitat habitat={h} animals={this.props.animals} />)}
             </div>
         )
     }
